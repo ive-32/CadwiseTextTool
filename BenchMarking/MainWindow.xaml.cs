fb9f9990df0294721ca379dfb934793913301494
ivan.savelyev <ivan.savelyev@umbrellait.com>
2023-05-14 09:22:40 +0700

    Add readme to BenchMarking

--- BenchMarking/MainWindow.xaml.cs
+++ BenchMarking/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 
 namespace BenchMarking
@@ -13,12 +16,24 @@ namespace BenchMarking
             InitializeComponent();
         }
 
-        private void BenchMark(object sender, RoutedEventArgs e)
+        private async void BenchMark(object sender, RoutedEventArgs e)
         {
-            var res = BenchmarkRunner.Run<TextRefinementTest>();
+            var progress = new ProgressBar();
+            progress.IsIndeterminate = true;
+            progress.SetValue(Grid.RowProperty, 0);
+            progress.Height = 20;
+            progress.Margin = new Thickness(20, 0, 20, 0);
+            MainGrid.Children.Add(progress);
+            StartTest.Visibility = Visibility.Hidden;
             
+
+            var res = await Task.Run(() => BenchmarkRunner.Run<TextRefinementTest>()); 
+            
+            MainGrid.Children.Remove(progress);
+            StartTest.Visibility = Visibility.Visible;
             Results.Navigate(res.ResultsDirectoryPath + "/BenchMarking.TextRefinementTest-report.html");
         }
 
+        
     }
 }
